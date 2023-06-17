33111f1809d05ebe59c0633ec3ac00b0a54a0ffa
Roshgard <adriansmc@gmail.com>
2023-06-17 13:32:23 -0600

    modulo de ram

--- modulos/cpu/moduloCPU.c
+++ modulos/cpu/moduloCPU.c
@@ -1,31 +1,39 @@
-#include <linux/sched.h>
+//Header obligatorio de todos los modulos
 #include <linux/module.h>
+//Header para usar KERN_INFO
 #include <linux/kernel.h>
+
+//Header para las funciones module_init y module_exit
 #include <linux/init.h>
+//Header necesario porque se usara proc_ops/file_operations
 #include <linux/proc_fs.h>
-#include <linux/uaccess.h>
+/* Header para usar la lib seq_file y manejar el archivo en /proc*/
 #include <linux/seq_file.h>
-#include <linux/hugetlb.h>
+
+/* Librerias para lectura de procesos */
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
+
 #include <linux/mm.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Modulo CPU");
-MODULE_AUTHOR("Kevin Josue Calderon Peraza");
+MODULE_AUTHOR("Adrian Molina");
 
-struct task_struct *cpu;     //Variable que almacena todos los procesos
-struct task_struct *child;   //Variable que almacena un proceso hijo
-struct list_head *nodo;      //Lista de procesos hijos
-int ram;                     //Porcentaje de RAM
+int ram; //Porcentaje de ram
 
-static int writeFile(struct seq_file *file, void *v) {
+static int writeFile(struct seq_file *file, void *v){
     int first = 0;
     int first2 = 0;
 
     seq_printf(file, "{ \"data\": [");
-    for_each_process(cpu) {
-        if (first == 0) {
+    struct task_struct * child; //Variable que almacena un proceso hijo
+    struct list_head * nodo; //Lista de procesos hijos
+    struct task_struct *cpu; //Variable que almacena un proceso padre
+    for_each_process(cpu){
+        if(first == 0){
             seq_printf(file, "{");
-        } else {
+        }else{
             seq_printf(file, ", {");
         }
         first += 1;
@@ -37,27 +45,29 @@ static int writeFile(struct seq_file *file, void *v) {
         seq_printf(file, "%s", cpu->comm);
         seq_printf(file, "\"");
         seq_printf(file, ", \"user\": ");
-        seq_printf(file, "%d", cpu->cred->uid.val);
+        seq_printf(file, "%d", cpu->real_cred->uid);
         seq_printf(file, ", \"state\": ");
-        seq_printf(file, "%ld", cpu->state);
+        seq_printf(file, "%d", cpu->state);
+
         if (cpu->mm) {
-            ram = (get_mm_rss(cpu->mm) << PAGE_SHIFT) / (1024 * 1024);
+            ram = (get_mm_rss(cpu->mm)<<PAGE_SHIFT)/(1024*1024);
             seq_printf(file, ", \"ram\": ");
             seq_printf(file, "%d", ram);
-        } else {
+        }else{
             seq_printf(file, ", \"ram\": ");
             seq_printf(file, "0");
         }
+
         seq_printf(file, ", \"children\": ");
         seq_printf(file, "[");
         first2 = 0;
 
-        list_for_each(nodo, &(cpu->children)) {
+        list_for_each(nodo, &(cpu->children)){
             child = list_entry(nodo, struct task_struct, sibling);
 
-            if (first2 == 0) {
+            if(first2 == 0){
                 seq_printf(file, "{");
-            } else {
+            }else{
                 seq_printf(file, ", {");
             }
             first2 += 1;
@@ -73,32 +83,37 @@ static int writeFile(struct seq_file *file, void *v) {
         seq_printf(file, "}");
     }
 
+
     seq_printf(file, "]}");
     return 0;
 }
 
-static int openFile(struct inode *inode, struct file *file) {
+//Funcion que se ejecutara cada vez que se lea el archivo con el comando CAT
+static int al_abrir(struct inode *inode, struct file *file)
+{
     return single_open(file, writeFile, NULL);
 }
 
-static const struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .open = openFile,
-    .read = seq_read,
-    .llseek = seq_lseek,
-    .release = single_release,
+//Si el kernel es menor al 5.6 usan file_operations
+static struct file_operations operaciones =
+{
+    .open = al_abrir,
+    .read = seq_read
 };
 
-static int insertMod(void) {
-    proc_create("cpu_grupo19", 0, NULL, &fops);
+//Funcion a ejecuta al insertar el modulo en el kernel con insmod
+static int _insert(void){
+    proc_create("cpu_grupo19", 0, NULL, &operaciones);
     printk(KERN_INFO "Hola mundo, somos el grupo 19 y este es el monitor de CPU\n");
     return 0;
 }
 
-static void removeMod(void) {
+
+//Funcion a ejecuta al remover el modulo del kernel con rmmod
+static void _remove(void){
     remove_proc_entry("cpu_grupo19", NULL);
-    printk(KERN_INFO "Sayonara mundo, somos el grupo 19 y este fue el monitor de CPU\n");
+    printk(KERN_INFO "Sayonara mundo, somos el grupo 19 y este fue monitor de CPU\n");
 }
 
-module_init(insertMod);
-module_exit(removeMod);
+module_init(_insert);
+module_exit(_remove);
---
+++ modulos/ram/moduloRAM.c
@@ -0,0 +1,61 @@
+#include <linux/module.h>
+// para usar KERN_INFO
+#include <linux/kernel.h>
+
+//Header para los macros module_init y module_exit
+#include <linux/init.h>
+//Header necesario porque se usara proc_fs
+#include <linux/proc_fs.h>
+/* for copy_from_user */
+#include <asm/uaccess.h>	
+/* Header para usar la lib seq_file y manejar el archivo en /proc*/
+#include <linux/seq_file.h>
+//librerias para memoria ram 
+
+#include <linux/mm.h>
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Modulo RAM");
+MODULE_AUTHOR("Adrian Molina");
+
+//Funcion que se ejecutara cada vez que se lea el archivo con el comando CAT
+static int escribir_archivo(struct seq_file *archivo, void *v)
+{   
+    struct sysinfo si;
+    si_meminfo(&si);
+    seq_printf(archivo, "{\"totalram\":\"%lu\", \n",(si.totalram * si.mem_unit)/1024);
+    seq_printf(archivo, "\"freeram\":\"%lu\"", (si.freeram * si.mem_unit)/1024);
+    seq_printf(archivo, "} \n");
+    return 0;
+}
+
+//Funcion que se ejecutara cada vez que se lea el archivo con el comando CAT
+static int al_abrir(struct inode *inode, struct file *file)
+{
+    return single_open(file, escribir_archivo, NULL);
+}
+
+//Si el kernel es 5.6 o mayor se usa la estructura proc_ops
+static struct file_operations operaciones =
+{
+    .open = al_abrir,
+    .read = seq_read
+};
+
+//Funcion a ejecuta al insertar el modulo en el kernel con insmod
+static int _insert(void)
+{
+    proc_create("ram_grupo19", 0, NULL, &operaciones);
+    printk(KERN_INFO "Hola mundo, somos el grupo 19 y este es el monitor de memoria\n");
+    return 0;
+}
+
+//Funcion a ejecuta al remover el modulo del kernel con rmmod
+static void _remove(void)
+{
+    remove_proc_entry("ram_grupo19", NULL);
+    printk(KERN_INFO "Sayonara mundo, somos el grupo 19 y este es el monitor de memoria\n");
+}
+
+module_init(_insert);
+module_exit(_remove);
