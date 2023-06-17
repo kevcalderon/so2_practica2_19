22d851aed27151cc15f0597161519942c0da058d
Roshgard <adriansmc@gmail.com>
2023-06-17 13:49:22 -0600

    modulo de ram

--- modulos/ram/moduloRAM.c
+++ modulos/ram/moduloRAM.c
@@ -1,25 +1,31 @@
+//Header obligatorio de todos los modulos
 #include <linux/module.h>
-// para usar KERN_INFO
+
+//Header para usar KERN_INFO
 #include <linux/kernel.h>
 
-//Header para los macros module_init y module_exit
+//Header para las funciones module_init y module_exit
 #include <linux/init.h>
-//Header necesario porque se usara proc_fs
+
+//Header necesario porque se usara proc_ops/file_operations
 #include <linux/proc_fs.h>
+
 /* for copy_from_user */
 #include <asm/uaccess.h>	
+
 /* Header para usar la lib seq_file y manejar el archivo en /proc*/
 #include <linux/seq_file.h>
-//librerias para memoria ram 
 
+/* Librerias para lectura de procesos */
 #include <linux/mm.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Modulo RAM");
 MODULE_AUTHOR("Adrian Molina");
 
-//Funcion que se ejecutara cada vez que se lea el archivo con el comando CAT
-static int escribir_archivo(struct seq_file *archivo, void *v)
+int ram; //Porcentaje de ram
+
+static int writeFile(struct seq_file *archivo, void *v)
 {   
     struct sysinfo si;
     si_meminfo(&si);
@@ -32,10 +38,10 @@ static int escribir_archivo(struct seq_file *archivo, void *v)
 //Funcion que se ejecutara cada vez que se lea el archivo con el comando CAT
 static int al_abrir(struct inode *inode, struct file *file)
 {
-    return single_open(file, escribir_archivo, NULL);
+    return single_open(file, writeFile, NULL);
 }
 
-//Si el kernel es 5.6 o mayor se usa la estructura proc_ops
+//Si el kernel es menor al 5.6 usan file_operations
 static struct file_operations operaciones =
 {
     .open = al_abrir,
@@ -43,18 +49,17 @@ static struct file_operations operaciones =
 };
 
 //Funcion a ejecuta al insertar el modulo en el kernel con insmod
-static int _insert(void)
-{
+static int _insert(void){
     proc_create("ram_grupo19", 0, NULL, &operaciones);
-    printk(KERN_INFO "Hola mundo, somos el grupo 19 y este es el monitor de memoria\n");
+    printk(KERN_INFO "Hola mundo, somos el grupo 19 y este es el monitor de RAM\n");
     return 0;
 }
 
+
 //Funcion a ejecuta al remover el modulo del kernel con rmmod
-static void _remove(void)
-{
+static void _remove(void){
     remove_proc_entry("ram_grupo19", NULL);
-    printk(KERN_INFO "Sayonara mundo, somos el grupo 19 y este es el monitor de memoria\n");
+    printk(KERN_INFO "Sayonara mundo, somos el grupo 19 y este fue monitor de RAM\n");
 }
 
 module_init(_insert);
